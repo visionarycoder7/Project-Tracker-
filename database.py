98fe7804f84d5836284ecc2fadd91b9ed9345abc
visionarycoder7 <143948847+visionarycoder7@users.noreply.github.com>
2026-03-31 23:20:38 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect("project.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+score INTEGER DEFAULT 0
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS tasks(
+task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+task TEXT,
+points INTEGER
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database Created Successfully")
